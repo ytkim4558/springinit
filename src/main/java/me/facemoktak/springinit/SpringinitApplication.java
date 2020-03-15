fe3009240753308abd9ac018ee47bb60f4e96787
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2020-03-15 15:48:21 +0900

    git initial

---
+++ src/main/java/me/facemoktak/springinit/SpringinitApplication.java
@@ -0,0 +1,13 @@
+package me.facemoktak.springinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/facemoktak/springinit/SpringinitApplicationTests.java
@@ -0,0 +1,13 @@
+package me.facemoktak.springinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
